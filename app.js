f797e11ac0d296fb8929fe1044e9e565b83f0fe7
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-11-10 09:58:08 -0600

    its fixed now not sure how but it is

--- app.js
+++ app.js
@@ -22,45 +22,42 @@ setTimeout(() => {
 
     }, 2000);
 
-    // 3a
-    let i=0
-    let time = setInterval(() => {
-        i += 1;
-        console.log(i);
-    }, 1000);
+   // 3a
+let i = 0;
+let time = setInterval(() => {
+    i += 1;
+    console.log(i);
+}, 1000);
 
-    // 3b
-    const btn = document.querySelector(`button`);
-    btn.addEventListener(`click`,() => {
-        clearinterval(time);
-    });
+// 3b
+const btn = document.querySelector(`button`);
+btn.addEventListener(`click`, () => {
+    clearInterval(time);
+});
 
     // bonus
 
     
 
     const div3 = document.querySelector(`#countdown`);
-    const p3 = document.createElement(`p`);
-    let num = 120;
-    const count2 = setInterval(() => {
-        if (num>0){
-            let minutes = math.floor(num/60);
-            let seconds = num - minutes * 60;
-
-            if (seconds == 0){
-                p3.innerText = `${minutes}:${seconds}0`;
-            } 
-            else if (seconds <10) {
-                p3.innerText=`${minutes}:0${seconds}`;
-            }
-            else {
+const p3 = document.createElement(`p`);
+let num = 120;
+const count2 = setInterval(() => {
+    if (num > 0){
+        let minutes = Math.floor(num/60);
+        let seconds = num - minutes * 60;
+        if (seconds == 0){
+            p3.innerText = `${minutes}:${seconds}0`;
+        } else if (seconds < 10) {
+            p3.innerText = `${minutes}:0${seconds}`;
+        } else {
             p3.innerText = `${minutes}:${seconds}`;
         }
-            div3.append(p3);
-        } else {
-            clearinterval(count2);
-            p3.innerText = `Time is Up!`
-            div3.append(p3);
-        }
-        num--;
-    },1000);
+        div3.append(p3);
+    } else {
+        clearInterval(count2);
+        p3.innerText = `TIME IS UP!`;
+        div3.append(p3);
+    }
+    num--;
+}, 1000);
